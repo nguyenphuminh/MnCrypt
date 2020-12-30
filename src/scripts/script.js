88f7c46734e5b50444382c660b53b60799e77a84
Phu Minh (FreakC) <53084840+nguyenphuminh@users.noreply.github.com>
2020-12-30 19:38:26 +0700

    Minor changes made

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,6 +6,7 @@ function encrypt(){
     let key=keyGen(), data=inp.value;
     for (i=65;i<=90;i++) data = data.replace(new RegExp(String.fromCharCode(i),'g'),(key+i).toString());
     for (i=97;i<=122;i++) data = data.replace(new RegExp(String.fromCharCode(i),'g'),(key*2+i).toString());
+    data = data.split("").reverse().join("");
     for (i=48;i<=57;i++) data = data.replace(new RegExp(String.fromCharCode(i),'g'),String.fromCharCode(i+20));
     let op = `Data: ${data}\nKey: ${key}`;
     out.innerHTML = op;
@@ -14,6 +15,7 @@ function encrypt(){
 function decrypt(){
     let data = inp.value, key = parseInt(kt.value);
     for (i=48;i<=57;i++) data = data.replace(new RegExp(String.fromCharCode(i+20),'g'),String.fromCharCode(i));
+    data = data.split("").reverse().join("");
     for (i=65;i<=90;i++) data = data.replace(new RegExp((key+i).toString(),'g'),String.fromCharCode(i));
     for (i=97;i<=122;i++) data = data.replace(new RegExp((key*2+i).toString(),'g'),String.fromCharCode(i));
     out.innerHTML = data;
